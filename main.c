64883d49a606b3b76ec6d5bbda7ca3181ba1a314
Your Name <you@example.com>
2024-02-15 08:39:47 +0200

    sync

--- main.c
+++ main.c
@@ -4,23 +4,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+
+typedef uint8_t direction_t;
 
 enum Block_type {
+  NONE = 0,
   SNAKE = 1,
   FOOD,
 };
 
-void displayBoard(uint8_t *board, int board_size, int block_size,
-                  int blocks_per_row) {
+enum Direction_type {
+  UP = 1,
+  DOWN,
+  LEFT,
+  RIGHT,
+};
+
+void move_snake(direction_t dir, int *head, int *tail, int new_tail,
+                uint8_t *board, int row_size) {
+  board[*tail] = NONE;
+  *tail = new_tail;
+  if (dir == UP) {
+    *head = *head - row_size;
+    board[*head] = SNAKE;
+  } else if (dir == DOWN) {
+    *head = *head + row_size;
+    board[*head] = SNAKE;
+  } else if (dir == LEFT) {
+    *head = *head - 1;
+    board[*head] = SNAKE;
+  } else if (dir == RIGHT) {
+    *head = *head + 1;
+    board[*head] = SNAKE;
+  }
+}
+
+direction_t get_input(direction_t cur) {
+  if (IsKeyPressed(KEY_UP)) {
+    return UP;
+  } else if (IsKeyPressed(KEY_DOWN)) {
+    return DOWN;
+  } else if (IsKeyPressed(KEY_LEFT)) {
+    return LEFT;
+  } else if (IsKeyPressed(KEY_RIGHT)) {
+    return RIGHT;
+  }
+  return cur;
+}
+
+int place_random_food(uint8_t *board, int board_size) {
+  int ans;
+  while (true) {
+    ans = GetRandomValue(0, board_size - 1);
+    if (board[ans] != SNAKE) {
+      board[ans] = FOOD;
+      break;
+    }
+  }
+  return ans;
+}
+
+void display_board(uint8_t *board, int board_size, int block_size,
+                   int blocks_per_row) {
   for (int i = 0; i <= board_size - 1; ++i) {
     Color color;
+    double roundness;
     switch (board[i]) {
     case SNAKE:
       color = GREEN;
+      roundness = .7f;
       break;
     case FOOD:
       color = RED;
+      roundness = 1.0f;
       break;
     default:
       continue;
@@ -30,12 +86,12 @@ void displayBoard(uint8_t *board, int board_size, int block_size,
     rect.y = (int)(i / blocks_per_row) * block_size;
     rect.width = block_size;
     rect.height = block_size;
-    DrawRectangleRounded(rect, .4f, 5, color);
+    DrawRectangleRounded(rect, roundness, 5, color);
   }
 }
 
 int main(int argc, char **argv) {
-  if (argc < 6 || argc > 6) {
+  if (argc < 5 || argc > 5) {
     fprintf(stderr,
             "too many or few arguments\nusage:\n\t%s <blocks x> <blocks y> "
             "<game speed> <snake length> <block size in pixels>\n",
@@ -43,34 +99,36 @@ int main(int argc, char **argv) {
     exit(EXIT_FAILURE);
   }
 
-  int speed, player_len, game_x, game_y, wx, wy, block_size, text_size,
-      board_size;
-
-  block_size = atoi(argv[5]);
-  player_len = atoi(argv[4]);
-  speed = atoi(argv[3]);
-  game_x = atoi(argv[1]);
-  game_y = atoi(argv[2]);
-  wx = game_x * block_size;
-  wy = game_y * block_size;
-  board_size = game_x * game_y;
-  text_size = (int)wy * 0.06;
+  int block_size = atoi(argv[4]);
+  int speed = atoi(argv[3]);
+  int speed_mod = 0;
+  int game_x = atoi(argv[1]);
+  int game_y = atoi(argv[2]);
+  int wx = game_x * block_size;
+  int wy = game_y * block_size;
+  int board_size = game_x * game_y;
+  int player_head = board_size / 2 - 1;
+  int player_tail = player_head;
+  int text_size = (int)wy * 0.06;
   uint8_t *board = malloc(board_size * sizeof(uint8_t));
+  direction_t dir = RIGHT;
 
-  board[0] = FOOD;
-  board[29] = FOOD;
-  board[board_size - 1] = SNAKE;
+  board[player_head] = SNAKE;
+  place_random_food(board, board_size);
 
   // for printing info about the starting values
 #ifndef NDEBUG
-  printf("arguments: %i, %i, %i, %i, %i\n", game_x, game_y, speed, player_len,
-         block_size);
+  printf("blocks: %i x %i, speed: %i, block size: %i, window: %i x %i, board "
+         "size: %i, text size: %i\n",
+         game_x, game_y, speed, block_size, wx, wy, board_size, text_size);
 #endif
 
   InitWindow(wx, wy, "snake game");
+  SetRandomSeed(42069);
   SetTargetFPS(60);
 
   bool playing = false;
+  // GAME LOOP
   while (!WindowShouldClose()) {
     // loop 1 for starting game
     while (!playing) {
@@ -86,16 +144,24 @@ int main(int argc, char **argv) {
       DrawText("press space to play", 10, 10, text_size, WHITE);
       EndDrawing();
     } // end of loop 1
+
     // game logic
+    dir = get_input(dir);
+    if (speed_mod > speed) {
+      speed_mod = 0;
+      move_snake(dir, &player_head, &player_tail, player_head, board, game_x);
+    }
+    ++speed_mod;
 
     // rendering
     BeginDrawing();
     ClearBackground(BLACK);
-    displayBoard(board, board_size, block_size, game_x);
+    display_board(board, board_size, block_size, game_x);
     EndDrawing();
-  }
+  } // end of GAME LOOP
 
 quit_game:
+  printf("-->%p\n", board);
   free(board);
   CloseWindow();
   return EXIT_SUCCESS;
--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
 export LD_LIBRARY_PATH=./
-./snake-game 30 25 5 1 30
+./snake-game 30 25 5 30
+# this script is used to set the lib path 
+# to the current working dir and then 
+# executing the game with "default" settings
