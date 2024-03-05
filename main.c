2adfaa1df781fd0dab0bf9a9dfe3746667038682
user <you@example.com>
2024-03-05 20:25:54 +0200

    added pause and score and other junk

--- main.c
+++ main.c
@@ -13,8 +13,10 @@ void draw_blocks(Vector2 *segms, int score, Vector2 cube_size,
   DrawRectangleV(Vector2Multiply(food_pos, cube_size), cube_size, RED);
 }
 
-void get_input(Vector2 *dir, Vector2 *coming) {
-  if (IsKeyDown(KEY_W) && coming->y != 1) {
+void get_input(Vector2 *dir, Vector2 *coming, bool *paused) {
+  if (IsKeyPressed(KEY_ESCAPE)) {
+    *paused = !*paused;
+  } else if (IsKeyDown(KEY_W) && coming->y != 1) {
     *dir = (Vector2){0, -1};
   } else if (IsKeyDown(KEY_A) && coming->x != 1) {
     *dir = (Vector2){-1, 0};
@@ -71,6 +73,12 @@ void game_over(int score) {
   }
 }
 
+void draw_score(int score) {
+  char str_score[32];
+  sprintf(str_score, "score: %i", score);
+  DrawText(str_score, 10, 10, 40, BLUE);
+}
+
 int main(void) {
   int block_side = 50;
   Vector2 block_size = {block_side, block_side};
@@ -87,18 +95,24 @@ int main(void) {
   segms[0] = head;
 
   InitWindow(window.x, window.y, "snake game");
+  SetExitKey(KEY_NULL);
   SetTargetFPS(60);
 
   place_food(&food_pos, blocks, segms, score);
 
+  bool paused = false;
   int update = 0;
   while (!WindowShouldClose()) {
     // updating and getting input
-    if (update > update_fr) {
+    if (update > update_fr && !paused) {
       update = 0;
       head = Vector2Add(dir, head);
       if (out_of_bounds(head, blocks) || self_collision_check(segms, score)) {
-        break;
+        game_over(score);
+        score = 0;
+        head = (Vector2){0, 0};
+        dir = (Vector2){1, 0};
+        place_food(&food_pos, blocks, segms, score);
       }
       if (Vector2Equals(head, food_pos)) {
         place_food(&food_pos, blocks, segms, score);
@@ -108,15 +122,18 @@ int main(void) {
       coming = dir;
     }
     ++update;
-    get_input(&dir, &coming);
+    get_input(&dir, &coming, &paused);
     // rendering
     BeginDrawing();
     ClearBackground(BLACK);
     draw_blocks(segms, score, block_size, food_pos);
+    if (paused) {
+      DrawText("Paused", 10, 10, 100, RED);
+    } else {
+      draw_score(score);
+    }
     EndDrawing();
   }
-
-  game_over(score);
 
   CloseWindow();
   return EXIT_SUCCESS;
